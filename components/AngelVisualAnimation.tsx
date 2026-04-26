f71f3831ab001386fc6681e89e140ec370e55f27
heccentor-cpu <heccentor@gmail.com>
2026-04-25 19:46:54 -0600

    first commit

---
+++ components/AngelVisualAnimation.tsx
@@ -0,0 +1,11 @@
+export default function AngelVisualAnimation() {
+  return (
+    <div className="relative w-full h-full min-h-[520px] overflow-hidden bg-bc-blue">
+      <img
+        src="/images/angel-independencia-tradehub-v3.jpg"
+        alt="Ángel de la Independencia TradeHub Consulting"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+    </div>
+  );
+}
